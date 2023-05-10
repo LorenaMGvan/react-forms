5a1691819505e49e161665a8cfa1c4b68dbf817a
LorenaMGvan <8186769+LorenaMGvan@users.noreply.github.com>
2023-05-09 18:29:16 -0600

    añadir sweetalert

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import Controlado from './components/Controlado';
-import Cat from './components/Cat';
+// import Controlado from './components/Controlado';
+import { useState } from 'react';
+import Formulario from './components/Formulario';
+import Todos from './components/Todos';
+// import Cat from './components/Cat';
+
+const initialStateTodos = [
+  {
+    id: 1,
+    title: 'tarea #1',
+    description: 'leer',
+    state: true,
+    prioridad: true
+  },
+  {
+    id: 2,
+    title: 'tarea #2',
+    description: 'jugar',
+    state: false,
+    prioridad: false
+  },
+  {
+    id: 3,
+    title: 'tarea #3',
+    description: 'Correr',
+    state: true,
+    prioridad: true
+  },
+];
 
 const App = () => {
+
+  const [todos, setTodos] = useState(initialStateTodos);
+
+
   return (
     <div className='container'>
-      <Cat />
       <h1>Formularios</h1>
-      <Controlado />
+      <Formulario />
+      <Todos todos={ todos }/>
     </div>
   )
 };
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable no-unused-vars */
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { useState } from "react";
+
+
+const Formulario = () => { 
+    // inicializamos los campos
+    // const [title, setTitle] = useState('');
+    // const [description, setDescription] = useState('');
+    // const [state, setState] = useState('pendiente');
+
+    // inicializamos el state como un Objeto para optimizar
+    const [todo, setTodo] = useState({
+        title: 'Campo 1',
+        description: 'Campo 2',
+        state: 'pendiente',
+        prioridad: false
+    });
+
+    const { title, description, state, prioridad } = todo;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if ( !title.trim() || !description.trim() ) {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Título y descripción son obligatorios',
+              });
+        }
+
+        console.log("Error");
+        console.log( title, description, title);
+    } 
+    
+    const handleChange = (e) => {
+        // console.log(e.target.value);
+        // console.log(e.target.name);
+
+        const {name, type, checked, value } = e.target
+        setTodo({
+            ...todo,
+            [name]: type === 'checkbox' ? checked : value,
+        });
+        //[e.target.name]: e.target.value
+
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input 
+                name="title" 
+                value={title}
+                type="text" 
+                placeholder="Ingrese todo" 
+                // onChange={ e => setTitle(e.target.value) }
+                // onChange={(e)=> setTodo({ ...todo, title: e.target.value})}
+                onChange={handleChange}
+                className="form-control mb-2"/>
+
+            <textarea 
+                name="description" 
+                value={description}
+                className="form-control mb-2" 
+                onChange={handleChange}
+                placeholder="Ingrese descripción"
+                ></textarea>
+
+            <div className="form-check">    
+                <input 
+                    id="prioridadCheck"
+                    type="checkbox"
+                    className="form-check-input"
+                    name="prioridad"
+                    checked={ prioridad }
+                    onChange={ handleChange }
+                    // onChange={ (e) => 
+                    //         setTodo({...todo, prioridad: e.target.checked}) 
+                    //     }
+                     />
+                <label htmlFor="prioridadCheck">Prioridad</label>
+            </div>
+
+
+            <select 
+                name="state" 
+                value={state}
+                onChange={handleChange}
+                className="form-select mb-2">
+                <option value="pendiente">Pendiente</option>
+                <option value="completado">Completado</option>
+            </select>
+
+            <button type="submit" className="btn btn-primary">
+                Agregar Todo
+            </button>
+
+            
+        </form>
+    ) };
+
+export default Formulario;
