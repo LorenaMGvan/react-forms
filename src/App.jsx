a990a51b28f20b1b3937384363353a0bc616d93f
LorenaMGvan <lorena.mgvan@hotmail.com>
2023-05-11 16:08:58 -0600

    añadir todos al localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
 // import Controlado from './components/Controlado';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Formulario from './components/Formulario';
 import Todos from './components/Todos';
 // import Cat from './components/Cat';
 
-const initialStateTodos = [
-  {
-    id: 1,
-    title: 'tarea #1',
-    description: 'leer',
-    state: true,
-    prioridad: true
-  },
-  {
-    id: 2,
-    title: 'tarea #2',
-    description: 'jugar',
-    state: false,
-    prioridad: false
-  },
-  {
-    id: 3,
-    title: 'tarea #3',
-    description: 'Correr',
-    state: true,
-    prioridad: true
-  },
-];
-
+// const initialStateTodos = [
+//   {
+//     id: 1,
+//     title: 'tarea #1',
+//     description: 'leer',
+//     state: true,
+//     prioridad: true
+//   },
+//   {
+//     id: 2,
+//     title: 'tarea #2',
+//     description: 'jugar',
+//     state: false,
+//     prioridad: false
+//   },
+//   {
+//     id: 3,
+//     title: 'tarea #3',
+//     description: 'Correr',
+//     state: true,
+//     prioridad: true
+//   },
+// ];
+const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const App = () => {   
 
   const [todos, setTodos] = useState(initialStateTodos);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    console.log("useEfect");
+  }, [todos]);
+    // cada que cambian los todos, se renderiza, o ejecuta el còdigo del useEfect 
+      // con los corchetes vacìos para que se ejecute "Solo una vez"
 
   const addTodo = todo => {
     setTodos([...todos, todo]);
